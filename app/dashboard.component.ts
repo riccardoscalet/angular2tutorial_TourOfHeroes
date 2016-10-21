4ef551b75533ec13204d8e9369a669cdd5757fe3
riccardoscalet <riccardo.scalet@gmail.com>
2016-10-21 18:09:39 +0200

    Created rudimentary dashboard.

--- app/dashboard.component.ts
+++ app/dashboard.component.ts
@@ -1,10 +1,30 @@
 import {
-    Component
+    Component,
+    OnInit
 } from '@angular/core';
+
+import {
+    Hero
+} from './model/hero';
+import {
+    HeroService
+} from './hero.service';
 
 @Component({
     selector: "my-dashboard",
-    templateUrl: "templates/heroDashboard.html"
+    templateUrl: "templates/heroDashboard.html",
+    providers: [HeroService]
 })
 
-export class DashboardComponent {}
+export class DashboardComponent {
+    heroes: Hero[] = [];
+
+    constructor(private heroService: HeroService) {}
+
+    ngOnInit(): void {
+        this.heroService.getHeroes()
+            .then(heroes => this.heroes = heroes.slice(0, 5));
+    }
+
+    gotoDetail(hero: Hero): void { /* not implemented yet */ }
+}

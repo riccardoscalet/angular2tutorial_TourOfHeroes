56768729c628d48a29a7f15e8afe577a6a53e504
riccardoscalet <riccardo.scalet@gmail.com>
2016-10-21 12:23:50 +0200

    Created hero list and styles.

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,18 +4,52 @@ import {
 
 @Component({
     selector: "my-app",
-    templateUrl: "templates/heroDetails.html"
+    templateUrl: "templates/heroList.html",
+    styleUrls: ["styles/heroes.css"]
 })
 
 export class AppComponent {
     title = "Tour of Heroes";
-    hero: Hero = {
-        id: 1,
-        name: "Banana God"
-    };
+    heroes = Heroes;
 }
 
 export class Hero {
     id: number;
     name: string;
 }
+
+
+const Heroes: Hero[] = [{
+    id: 1,
+    name: "Banana God"
+}, {
+    id: 11,
+    name: 'Mr. Nice'
+}, {
+    id: 12,
+    name: 'Narco'
+}, {
+    id: 13,
+    name: 'Bombasto'
+}, {
+    id: 14,
+    name: 'Celeritas'
+}, {
+    id: 15,
+    name: 'Magneta'
+}, {
+    id: 16,
+    name: 'RubberMan'
+}, {
+    id: 17,
+    name: 'Dynama'
+}, {
+    id: 18,
+    name: 'Dr IQ'
+}, {
+    id: 19,
+    name: 'Magma'
+}, {
+    id: 20,
+    name: 'Tornado'
+}];

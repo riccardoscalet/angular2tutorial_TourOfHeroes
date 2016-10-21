62fd16787cdc5a564025448e3356292317cb1ae9
riccardoscalet <riccardo.scalet@gmail.com>
2016-10-21 11:41:49 +0200

    Added a super super hero.

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,8 +3,19 @@ import {
 } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    template: '<h1>My First Angular App</h1>'
+    selector: "my-app",
+    templateUrl: "templates/heroDetails.html"
 })
 
-export class AppComponent {}
+export class AppComponent {
+    title = "Tour of Heroes";
+    hero: Hero = {
+        id: 1,
+        name: "Banana God"
+    };
+}
+
+export class Hero {
+    id: number;
+    name: string;
+}

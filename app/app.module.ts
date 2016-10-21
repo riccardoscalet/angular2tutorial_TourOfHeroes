14fa6160ae1cd6e3cc2af6395536a37fb48543e3
riccardoscalet <riccardo.scalet@gmail.com>
2016-10-21 12:02:11 +0200

    Completed Banana God hero that is cool as fuck.

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,11 +5,18 @@ import {
     BrowserModule
 } from '@angular/platform-browser';
 import {
+    FormsModule
+} from '@angular/forms';
+
+import {
     AppComponent
 } from './app.component';
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
     declarations: [AppComponent],
     bootstrap: [AppComponent]
 })

3799ca80aa8bf4ac919b30dfb36de6cfa96ace7b
riccardoscalet <riccardo.scalet@gmail.com>
2016-11-09 11:46:33 +0100

    Fixes.

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,9 @@
 // Entry point of the application (defined in systemjs.config.js).
 
+// Imports required for TypeScript to work with Angular2
+import "zone.js";
+import "reflect-metadata";
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 

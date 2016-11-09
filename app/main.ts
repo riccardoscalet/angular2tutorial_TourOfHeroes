c7b51ebef55bc737bfad42f8c8c559e2766f0fa8
riccardoscalet <riccardo.scalet@gmail.com>
2016-11-09 14:26:05 +0100

    (Small note on start).

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,6 @@
+// START - Use 'npm start' from console to run this angular2 application.
+// (I haven't found a way to make it work directly through VSCode, yet)
+
 // Entry point of the application (defined in systemjs.config.js).
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
